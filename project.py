ce972c2034cdd2f4ce67c4822ffd1130b3ac0724
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2019-12-28 17:32:42 +0100

    Update project.py

--- project.py
+++ project.py
@@ -67,15 +67,19 @@ def list_hover(e):
             pass
         w.Xhover_i = i
     #print ('hover',i)
-    w.itemconfig(i,background = 'gray80')
+    w.itemconfig(i,background = 'yellow') #gray80
 
 def list_leave(e):
     w=e.widget
     w.itemconfig(w.Xhover_i,background = 'white')
 
 
-
 ##U+2589
+
+def Appear(x,manager,**kw):
+    fun = getattr(x,manager)
+
+    fun(x,**kw)
 
 #detour = lambda i: visible(top)
 #top.bind('<Visibility>',detour)
@@ -86,38 +90,43 @@ image = tk_image('test1.jpg')
 ImageFrame = make_frame(top,'Image')
 
 
-c = t.Canvas(top,) # cursor = 'hand2'
+c = t.Canvas(top) # cursor = 'hand2'
+
+
 l = t.Label(ImageFrame,image = image)
 
 ImageFrame.grid(row=0,column=1, padx = 10)
 l.grid(row=0,column=1)
+
 #c.grid(row=0,column=2)
 
 
 CurrentFrame = make_frame(top,'Images in Current Path',-2)
-CurrentFrame.grid(row = 0, column = 2,  padx = 10)
+CurrentFrame.grid(row = 0, column = 0,  padx = 10)
 
 ScrollFrame = t.Frame(CurrentFrame,relief='groove')
 
 
 
-scroll1 = t.Scrollbar(ScrollFrame,orient=t.VERTICAL)
-scroll2 = t.Scrollbar(ScrollFrame,orient=t.HORIZONTAL)
-scroll1.pack(side= t.RIGHT, fill = t.Y)
-scroll2.pack(side= t.BOTTOM, fill = t.X)
+list_scroll1 = t.Scrollbar(ScrollFrame,orient=t.VERTICAL)
+list_scroll2 = t.Scrollbar(ScrollFrame,orient=t.HORIZONTAL)
+list_scroll1.pack(side= t.RIGHT, fill = t.Y)
+list_scroll2.pack(side= t.BOTTOM, fill = t.X)
+
 
 #
 # icon from Gemicon Icon Set (600+ free icons) (http://gemicon.net)
 #
 #l1 = t.Label(frame2,text='Files in Current Directory')       ; l1.pack(side = t.LEFT)
 png = tk_image('folder3.png',20,20) ;
-list1 = t.Listbox(ScrollFrame, yscrollcommand = scroll1.set,xscrollcommand = scroll2.set, exportselection=0, selectmode = t.SINGLE, relief = 'groove',activestyle='none',selectbackground = 'steelblue2',selectforeground='black', cursor = 'hand2' )
+list1 = t.Listbox(ScrollFrame, yscrollcommand = list_scroll1.set,xscrollcommand = list_scroll2.set, exportselection=0, selectmode = t.SINGLE, relief = 'groove',activestyle='none',selectbackground = 'steelblue2',selectforeground='black', cursor = 'hand2' )
 list1.pack(side = t.LEFT, fill= t.BOTH)
+
 b1 = t.Button(CurrentFrame,image =  png, relief = 'groove', command = get_dir)
 b1.pack(side=t.TOP,anchor= t.E, pady = 5)
 ScrollFrame.pack(side = t.BOTTOM)
-scroll1.config(command = list1.yview)
-scroll2.config(command = list1.xview)
+list_scroll1.config(command = list1.yview)
+list_scroll2.config(command = list1.xview)
 list1.focus()
 list1.Xpath = os.getcwd()
 populate_list()
