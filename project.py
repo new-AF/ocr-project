a8b1114a334ecd0f6d847d2922eafb9fe05c6e3e
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2019-12-28 10:26:33 +0100

    Update project.py
    Functions as image viewer

--- project.py
+++ project.py
@@ -15,18 +15,63 @@ def get_image_names(x):
     x = filter(lambda l:l.rpartition('.')[2] in ['png','tif','tiff','jpg','jpeg','gif'],x)
     return x
 
-def populate_list(x):
+def populate_list(x = None):
+    if x == None:
+        x = list1.Xpath
     y = get_image_names(x)
+
     for j,i in enumerate(y):
         list1.insert(j+1,i)
 
+
+def tk_image(fname,*rest ):
+    p = Image.open(fname) # pillow_image
+
+    if rest:
+        p = p.resize(rest[0:2])
+
+    i = ImageTk.PhotoImage(p)
+    return i
+
+def list_select(e):
+    global image
+    w = e.widget
+    #print dir(e)
+    i = w.curselection()[0]
+
+    text = w.get(i) #; print (text)
+    path = os.path.join(list1.Xpath,text)
+    #print (path)
+    image = tk_image(path)
+    l.configure(image = image)
+    #print ()
+
+def list_hover(e):
+    #print (dir(e))
+    w=e.widget
+
+    s= w.curselection()[0:1]
+    i=w.nearest(e.y)
+
+    if s!= i:
+        try:
+            w.itemconfig(w.Xhover_i,background = 'white')
+        except:
+            pass
+        w.Xhover_i = i
+    #print ('hover',i)
+    w.itemconfig(i,background = 'gray80')
+
+def list_leave(e):
+    w=e.widget
+    w.itemconfig(w.Xhover_i,background = 'white')
 ##U+2589
 
 #detour = lambda i: visible(top)
 #top.bind('<Visibility>',detour)
 
 #top.tk_bisque()
-image = ImageTk.PhotoImage(Image.open('test1.jpg'))
+image = tk_image('test1.jpg')
 
 c = t.Canvas(top,) # cursor = 'hand2'
 l = t.Label(top,image = image)
@@ -41,10 +86,15 @@ frame2 = t.Frame(frame1)                    ; frame2.pack(side = t.TOP)
 #
 # icon from Gemicon Icon Set (600+ free icons) (http://gemicon.net)
 #
-l1 = t.Label(frame2,text='Files in Current Directory')       ; l1.pack(side = t.LEFT) ; png = ImageTk.PhotoImage(Image.open('folder3.png')) ;
-list1 = t.Listbox(frame1, selectmode = t.SINGLE, relief = 'groove',activestyle='none',selectbackground = 'yellow',selectforeground='black')     ;  list1.pack(side = t.BOTTOM, fill= t.X)
-b1 = t.Button(frame2,image =  png, cursor = 'hand2',relief = 'groove', command = get_dir)     ; b1.pack(side=t.RIGHT) ; b1.config(padx = 11)
+l1 = t.Label(frame2,text='Files in Current Directory')       ; l1.pack(side = t.LEFT) ; png = tk_image('folder3.png',20,20) ;
+list1 = t.Listbox(frame1,exportselection=0, selectmode = t.SINGLE, relief = 'groove',activestyle='none',selectbackground = 'steelblue2',selectforeground='black', cursor = 'hand2' )     ;  list1.pack(side = t.BOTTOM, fill= t.X)
+b1 = t.Button(frame2,image =  png, relief = 'groove', command = get_dir)     ; b1.pack(side=t.RIGHT) ; b1.config(padx = 11)
 
-populate_list(os.getcwd())
+list1.focus()
+list1.Xpath = os.getcwd()
+populate_list()
+list1.bind('<<ListboxSelect>>',list_select)
+list1.bind('<Motion>',list_hover)
+list1.bind('<Leave>',list_leave)
 top.geometry('800x800+{}+{}'.format(1920/2,1080/2))
 top.mainloop()
