697d91dafd55c48bb38603d4420e2fd3428cea66
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2019-12-28 23:33:04 +0100

    Update project.py
    tkinter Text as Image Viewer

--- project.py
+++ project.py
@@ -16,16 +16,20 @@ def make_frame(parent,label,font_size_increment = 0):
 def get_dir():
     f = tkFileDialog.askdirectory(title = 'Choose the Path or Folder where Images reside')
     print (f)
+    populate_list(f)
 
 def get_image_names(x):
     x = os.listdir(x)
     x = filter(lambda l:l.rpartition('.')[2] in ['png','tif','tiff','jpg','jpeg','gif'],x)
     return x
 
-def populate_list(x = None):
-    if x == None:
-        x = list1.Xpath
+def populate_list(x = False):
+    if not x:
+        x = os.getcwd()
+    list1.Xpath = x
     y = get_image_names(x)
+
+    list1.delete(0,t.END)
 
     for j,i in enumerate(y):
         list1.insert(j+1,i)
@@ -41,7 +45,7 @@ def tk_image(fname,*rest ):
     return i
 
 def list_select(e):
-    global image
+    global image,VirtualImage
     w = e.widget
     #print dir(e)
     i = w.curselection()[0]
@@ -50,7 +54,9 @@ def list_select(e):
     path = os.path.join(list1.Xpath,text)
     #print (path)
     image = tk_image(path)
-    l.configure(image = image)
+    ImageText.delete(VirtualImage)
+    VirtualImage = ImageText.image_create(t.END,image = image)
+    #l.configure(image = image)
     #print ()
 
 def list_hover(e):
@@ -85,29 +91,36 @@ def Appear(x,manager,**kw):
 #top.bind('<Visibility>',detour)
 
 #top.tk_bisque()
+
+## Variables
 image = tk_image('test1.jpg')
-
 ImageFrame = make_frame(top,'Image')
-
-
-c = t.Canvas(top) # cursor = 'hand2'
-
-
-l = t.Label(ImageFrame,image = image)
-
-ImageFrame.grid(row=0,column=1, padx = 10)
-l.grid(row=0,column=1)
-
-#c.grid(row=0,column=2)
-
-
+ImageText = t.Text(ImageFrame)
 CurrentFrame = make_frame(top,'Images in Current Path',-2)
-CurrentFrame.grid(row = 0, column = 0,  padx = 10)
-
 ScrollFrame = t.Frame(CurrentFrame,relief='groove')
+VirtualImage = ImageText.image_create(t.END, image=image)
+
+ImageTextScroll1 = t.Scrollbar(ImageFrame)
+ImageTextScroll2 = t.Scrollbar(ImageFrame,orient=t.HORIZONTAL)
+
+# Configs1
+CurrentFrame.place(relx=.05,rely=0.05)
+ImageFrame.place(relx=.3,rely=0.05,relwidth = .6, relheight = .5)
+#ImageFrame.grid(row=0,column=1, sticky='we')
+#ImageFrame.config(width=500,height=400)
+ImageText.place(x=0,y=0, relwidth=.95,relheight=.95)
+ImageText.config(yscrollcommand = ImageTextScroll1.set,xscrollcommand = ImageTextScroll2.set)
+ImageText['bg']=top['bg']
+#ImageFrame['']
 
 
+## Configs2
+ImageTextScroll1.config(command= ImageText.yview)
+ImageTextScroll2.config(command= ImageText.xview)
+ImageTextScroll1.place(relx=.95,y=0,relheight = .95)
+ImageTextScroll2.place(x=0,rely=0.95,relwidth=.95)
 
+## Old Variables
 list_scroll1 = t.Scrollbar(ScrollFrame,orient=t.VERTICAL)
 list_scroll2 = t.Scrollbar(ScrollFrame,orient=t.HORIZONTAL)
 list_scroll1.pack(side= t.RIGHT, fill = t.Y)
@@ -122,13 +135,14 @@ png = tk_image('folder3.png',20,20) ;
 list1 = t.Listbox(ScrollFrame, yscrollcommand = list_scroll1.set,xscrollcommand = list_scroll2.set, exportselection=0, selectmode = t.SINGLE, relief = 'groove',activestyle='none',selectbackground = 'steelblue2',selectforeground='black', cursor = 'hand2' )
 list1.pack(side = t.LEFT, fill= t.BOTH)
 
+
+
 b1 = t.Button(CurrentFrame,image =  png, relief = 'groove', command = get_dir)
 b1.pack(side=t.TOP,anchor= t.E, pady = 5)
 ScrollFrame.pack(side = t.BOTTOM)
 list_scroll1.config(command = list1.yview)
 list_scroll2.config(command = list1.xview)
 list1.focus()
-list1.Xpath = os.getcwd()
 populate_list()
 list1.bind('<<ListboxSelect>>',list_select)
 list1.bind('<Motion>',list_hover)
